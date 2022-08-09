cc5a31676f3b10085d6e3ce5e9dbd07baa7297ea
kehindeajibade <110301566+kehindeajibade@users.noreply.github.com>
2022-08-08 19:24:47 -0500

    Add files via upload

---
+++ carlys_clippers_code.py
@@ -0,0 +1,33 @@
+# Carly's Clippers Python Project code
+
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+
+for price in prices:
+  total_price += price
+# print(total_price)
+
+average_price = total_price / len(prices)
+print('Average Haircut Price:',average_price)
+
+new_prices = [new_price - 5 for new_price in prices]
+print(new_prices)
+
+total_revenue = 0
+
+for i in range(0, len(hairstyles)):
+  total_revenue += prices[i] * last_week[i]
+print('Total Revenue:',total_revenue)
+
+average_daily_revenue = total_revenue / 7
+print(average_daily_revenue)
+
+cuts_under_30 = [hairstyles[i] for i in range(len(hairstyles)) if new_prices[i] < 30]
+print('Hairstyles less than $30 : ',cuts_under_30)
+
+
